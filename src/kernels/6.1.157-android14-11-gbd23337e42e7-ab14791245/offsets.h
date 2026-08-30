419c0a51b263e856eb7a4b9e6ba44f1a8988aa55
Boy Prakasa <boyprakasa@gmail.com>
2026-08-30 22:23:09 +0700

    change hash

--- src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
+++ src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
@@ -4,20 +4,20 @@
 OFFSETS_ENTRY(
     "6.1.157-android14-11-gbd23337e42e7-ab14791245",
     STRUCT_OFFSETS_6_1,
-    .pselect_waiter_shift = 1,
-    .off_security_hook_heads = 0x015ce478,
-    .off_selinux_blob_sizes = 0x015ceb88,
-    .off_selinux_enforcing = 0x01fca8c0,
-    .off_slide_loggers_0_1 = 0x02012918,
-    .off_slide_nfulnl_logger = 0x020129d0,
-    .off_init_task = 0x0201f640,
-    .off_init_cred = 0x02031aa8,
+    .pselect_waiter_shift = -2,
+    .off_init_task = 0x0201F640,
+    .off_init_cred = 0x02031AA8,
     .off_root_task_group = 0x02208580,
-    .off_slide_boot_id = 0x0227b498, 
+    .off_selinux_enforcing = 0x01FCA8C0,
+    .off_selinux_blob_sizes = 0x015CEB88,
+    .off_security_hook_heads = 0x015CE478,
+    .off_slide_nfulnl_logger = 0x020129D0,
+    .off_slide_boot_id = 0x00000000,
+    .off_slide_loggers_0_1 = 0x02012918,
 ),
-/* BTF reference (runtime uses target.h defaults): */
-/* #define STRUCT_PAGE_SIZE 0x40 */
-/* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
-/* #define STRUCT_PAGE_TYPE 0x30 */
-/* #define STRUCT_SLAB_CACHE 0x8 */
-/* #define STRUCT_MM_STRUCT 0x4C0 */
+    /* BTF reference (runtime uses target.h defaults): */
+    /* #define STRUCT_PAGE_SIZE 0x40 */
+    /* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
+    /* #define STRUCT_PAGE_TYPE 0x30 */
+    /* #define STRUCT_SLAB_CACHE 0x8 */
+    /* #define STRUCT_MM_STRUCT 0x4C0 */
