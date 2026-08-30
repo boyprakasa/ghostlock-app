284ba20e027b2aea0d235909ff3e42a9994b0168
Boy Prakasa <boyprakasa@gmail.com>
2026-08-30 22:31:53 +0700

    change hash

--- src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
+++ src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
@@ -4,7 +4,7 @@
 OFFSETS_ENTRY(
     "6.1.157-android14-11-gbd23337e42e7-ab14791245",
     STRUCT_OFFSETS_6_1,
-    .pselect_waiter_shift = -2,
+    .pselect_waiter_shift = 1,
     .off_init_task = 0x0201F640,
     .off_init_cred = 0x02031AA8,
     .off_root_task_group = 0x02208580,
@@ -12,7 +12,7 @@ OFFSETS_ENTRY(
     .off_selinux_blob_sizes = 0x015CEB88,
     .off_security_hook_heads = 0x015CE478,
     .off_slide_nfulnl_logger = 0x020129D0,
-    .off_slide_boot_id = 0x00000000,
+    .off_slide_boot_id = 0x0227B498,
     .off_slide_loggers_0_1 = 0x02012918,
 ),
     /* BTF reference (runtime uses target.h defaults): */
