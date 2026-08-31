613064333f3302096ed2bb5f5412d1cebddc5918
Wiranda Boy Prakasa <boyprakasa@gmail.com>
2026-08-31 20:01:39 +0700

    Update offsets in offsets.h for kernel 6.1.157

--- src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
+++ src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
@@ -5,15 +5,15 @@ OFFSETS_ENTRY(
     "6.1.157-android14-11-gbd23337e42e7-ab14791245",
     STRUCT_OFFSETS_6_1,
     .pselect_waiter_shift = 1,
-    .off_init_task             = 0x0201F640,
-    .off_init_cred             = 0x02031AA8,
-    .off_root_task_group       = 0x02208580,
-    .off_selinux_enforcing     = 0x0225A420,   /* selinux_state (was: 0x01FCA8C0 selinux_enforcing_boot) */
-    .off_selinux_blob_sizes    = 0x015CEB88,
-    .off_security_hook_heads   = 0x015CE478,
-    .off_slide_nfulnl_logger   = 0x020129D0,
-    .off_slide_boot_id         = 0x0227B498,
-    .off_slide_loggers_0_1     = 0x02012918,
+    .off_init_task = 0x0201F640,
+    .off_init_cred = 0x02031AA8,
+    .off_root_task_group = 0x02208580,
+    .off_selinux_enforcing = 0x0225A420,
+    .off_selinux_blob_sizes = 0x015CEB88,
+    .off_security_hook_heads = 0x015CE478,
+    .off_slide_nfulnl_logger = 0x020129D0,
+    .off_slide_boot_id = 0x0227B498,
+    .off_slide_loggers_0_1 = 0x02012918
 ),
     /* BTF reference (runtime uses target.h defaults): */
     /* #define STRUCT_PAGE_SIZE 0x40 */
