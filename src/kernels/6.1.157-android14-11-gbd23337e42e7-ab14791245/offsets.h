87fa6dc8db7ae996c3848a16dfa4d1446922e1d7
Wiranda Boy Prakasa <boyprakasa@gmail.com>
2026-09-03 09:18:17 +0700

    Modify offsets for 6.1.157-android14-11
    Updated offsets for Google Pixel 6 Pro in offsets.h.

--- src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
+++ src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
@@ -2,18 +2,18 @@
  * Anchors from the boot.img (kallsyms + BTF). */
 
 OFFSETS_ENTRY(
-"6.1.157-android14-11-gbd23337e42e7-ab14791245",
-STRUCT_OFFSETS_6_1,
-.pselect_waiter_shift = 1,
-.off_init_task = 0x0201f640,
-.off_init_cred = 0x02031aa8,
-.off_root_task_group = 0x02208580,
-.off_selinux_enforcing = 0x01fca8c0,
-.off_selinux_blob_sizes = 0x015ceb88,
-.off_security_hook_heads = 0x015ce478,
-.off_slide_nfulnl_logger = 0x020129d0,
-.off_slide_boot_id = 0x0227b498,
-.off_slide_loggers_0_1 = 0x02012918,
+    "6.1.157-android14-11-gbd23337e42e7-ab14791245",
+    STRUCT_OFFSETS_6_1,
+    .pselect_waiter_shift = 1,
+    .off_init_task = 0x0201F640,
+    .off_init_cred = 0x02031AA8,
+    .off_root_task_group = 0x02208580,
+    .off_selinux_enforcing = 0x0225A420,
+    .off_selinux_blob_sizes = 0x015CEB88,
+    .off_security_hook_heads = 0x015CE478,
+    .off_slide_nfulnl_logger = 0x020129D0,
+    .off_slide_boot_id = 0x0227B498,
+    .off_slide_loggers_0_1 = 0x02012918,
 ),
 
 /* BTF reference for fields kept as target.h macros; the task, cred and
